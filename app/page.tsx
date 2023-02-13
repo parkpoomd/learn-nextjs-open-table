a0faf4066c82cfcb6107495cd97dbbd5de2bedab
parkpoomd <parkpoom.d@gmail.com>
2023-02-13 19:55:03 +0700

    feature: all components data

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,7 @@
 import { Inter } from "@next/font/google";
 import Header from "./components/Header";
 import RestaurantCard from "./components/RestaurantCard";
-import { PrismaClient, Cuisine, Location, PRICE } from "@prisma/client";
+import { PrismaClient, Cuisine, Location, PRICE, Review } from "@prisma/client";
 
 export interface RestaurantCardType {
   id: number;
@@ -11,6 +11,7 @@ export interface RestaurantCardType {
   location: Location;
   price: PRICE;
   slug: string;
+  reviews: Review[];
 }
 
 const prisma = new PrismaClient();
@@ -25,6 +26,7 @@ const fetchRestaurants = async (): Promise<RestaurantCardType[]> => {
       slug: true,
       location: true,
       price: true,
+      reviews: true,
     },
   });
 
